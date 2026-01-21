7e456fb4e3f9dd4f8b4bb99774be2c6df7167bc2
harshal710 <hspatil222002@gmail.com>
2026-01-21 18:58:38 +0530

    setup starter backend

---
+++ residentia-backend/src/main/java/com/example/demo/ResidentiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidentiaBackendApplication.class, args);
+	}
+
+}
